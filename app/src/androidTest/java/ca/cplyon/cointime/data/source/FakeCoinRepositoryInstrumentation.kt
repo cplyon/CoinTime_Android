7418c623ff50d9d3162af3dbe8431d129bef83db
Chris Lyon <chris@cplyon.ca>
2020-09-28 13:44:24 -0700

    Add instrumentation test for CoinListViewModel
    Simple unit-test that requires instrumentation due to the fact it's
    testing for a Bitmap, whose class isn't present in the unit test lib.

---
+++ app/src/androidTest/java/ca/cplyon/cointime/data/source/FakeCoinRepositoryInstrumentation.kt
@@ -0,0 +1,58 @@
+package ca.cplyon.cointime.data.source
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ca.cplyon.cointime.data.Coin
+import ca.cplyon.cointime.data.Result
+
+class FakeCoinRepositoryInstrumentation(coins: List<Coin>) : CoinRepository {
+
+    private var shouldReturnError = false
+    private var result: Result<List<Coin>> = Result.Success(coins)
+    private var bitmap: Bitmap? = null
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+        if (value) {
+            result = Result.Error(Exception("Fake exception"))
+            bitmap = null
+        } else {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+        }
+
+    }
+
+    override suspend fun getAllCoins(): Result<List<Coin>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addCoin(coin: Coin): Long {
+        return coin.coinId
+    }
+
+    override fun observeCoins(): LiveData<Result<List<Coin>>> {
+        return MutableLiveData(result)
+    }
+
+    override suspend fun deleteCoin(coin: Coin) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateCoin(coin: Coin) {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveImage(context: Context, image: Bitmap, suffix: String): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadImage(path: String): Bitmap? {
+        return bitmap
+    }
+
+    override fun deleteImage(path: String) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/androidTest/java/ca/cplyon/cointime/ui/main/CoinListViewModelInstrumentationTest.kt
@@ -0,0 +1,30 @@
+package ca.cplyon.cointime.ui.main
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ca.cplyon.cointime.data.Coin
+import ca.cplyon.cointime.data.source.FakeCoinRepositoryInstrumentation
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CoinListViewModelInstrumentationTest {
+
+    private lateinit var fakeRepository: FakeCoinRepositoryInstrumentation
+    private lateinit var vm: CoinListViewModel
+    private val coins = listOf(Coin("Dummy", "Dummy", 0, "Dummy", "Dummy"))
+
+    @Before
+    fun setUpViewModel() {
+        fakeRepository = FakeCoinRepositoryInstrumentation(coins)
+    }
+
+    @Test
+    fun loadImage_success() {
+        fakeRepository.setReturnError(false)
+        vm = CoinListViewModel(fakeRepository)
+        Assert.assertNotNull(vm.loadImage("fake_path"))
+    }
+
+}
