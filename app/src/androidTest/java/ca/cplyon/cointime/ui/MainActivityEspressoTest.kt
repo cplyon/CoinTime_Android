b72ab7b752c0123ff884ba2473ed9277c68ecaf4
Chris Lyon <chris@cplyon.ca>
2020-12-15 09:28:46 -0800

    Fix linter errors
    Forgot to remove unneeded imports when I commented out the Espresso
    test.

--- app/src/androidTest/java/ca/cplyon/cointime/ui/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/ca/cplyon/cointime/ui/MainActivityEspressoTest.kt
@@ -1,17 +1,12 @@
 package ca.cplyon.cointime.ui
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
